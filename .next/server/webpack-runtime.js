fc9e629d87368eb65a40ce27450b645812afc3a2
RaoAkif <akifrao@gmail.com>
2024-02-06 00:36:21 +0500

    Remove comment

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("fd68bccdac0a9b0c")
+/******/ 		__webpack_require__.h = () => ("b17b7065ebc30e8f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f981b8ebf891b15"; }
+/******/ 		__webpack_require__.h = function() { return "956d6acb59ec7f23"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
